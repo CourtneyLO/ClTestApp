9b7886006d671e2395a3fa09b4c73fd464902a02
Courtney Osborn <courtney.osborn@bcgdv.com>
2024-10-22 14:08:32 +0100

    Adds CSRF_TRUSTED_ORIGINS

--- main/settings.py
+++ main/settings.py
@@ -33,6 +33,8 @@ DEBUG = os.getenv('DEBUG') == 'True'
 URL_HOSTS = os.getenv('URL_HOSTS')
 ALLOWED_HOSTS = URL_HOSTS.split(', ');
 
+if os.getenv('CSRF_TRUSTED_ORIGINS'):
+    CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(', ')
 
 # CORS
 # https://github.com/adamchainz/django-cors-headers
