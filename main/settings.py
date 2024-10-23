6bb2a04248e75bcc971864e022f3c6970fdb1323
Courtney Osborn <courtney.osborn@bcgdv.com>
2024-10-23 10:25:59 +0100

    Change split to happen on just a comma

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 URL_HOSTS = os.getenv('URL_HOSTS')
-ALLOWED_HOSTS = URL_HOSTS.split(', ');
+ALLOWED_HOSTS = URL_HOSTS.split(',');
 
 if os.getenv('CSRF_TRUSTED_ORIGINS'):
     CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(', ')
